a90d3ad1b6e4cc5b18abab74d15df18ba2e75fbc
FurqatMashrabjonov <php_lesson@mail.ru>
2025-08-30 22:56:25 +0500

    modules

--- main.py
+++ main.py
@@ -1,3 +1,17 @@
-import module
+import math
+import os
+import random
+import sys
+import time
+from module import add
 
-print(module.add(32,13,123,123,21,3))
+# print(math.sqrt(25))
+# print(os.getcwd())
+# print(os.listdir('.'))
+# print(random.randrange(5, 9))
+# print(random.choice([21,321,3,123,12,3]))
+# print ("My name is {}. I am {} years old".format(sys.argv[1], sys.argv[2]))
+# print(time.localtime())
+# _sum = add(2323,324,23,423,4,234,32)
+# print(_sum)
+# print(dir(random))
