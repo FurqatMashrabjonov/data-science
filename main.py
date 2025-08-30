e2762dd163a2e0e93df94b4dc1dc57ce1caaeb71
FurqatMashrabjonov <php_lesson@mail.ru>
2025-08-30 23:16:15 +0500

    lambda functions

--- main.py
+++ main.py
@@ -1,17 +1,8 @@
 import math
-import os
-import random
-import sys
-import time
-from module import add
 
-# print(math.sqrt(25))
-# print(os.getcwd())
-# print(os.listdir('.'))
-# print(random.randrange(5, 9))
-# print(random.choice([21,321,3,123,12,3]))
-# print ("My name is {}. I am {} years old".format(sys.argv[1], sys.argv[2]))
-# print(time.localtime())
-# _sum = add(2323,324,23,423,4,234,32)
-# print(_sum)
-# print(dir(random))
+# s = (lambda a, b : a + b)(234, 4324)
+# print(s)
+
+s = lambda a, func: a + func(a)
+
+print(s(12, lambda a: int(math.pow(a, 2))))
